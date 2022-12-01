2a2b9f13779815c8273a8279bc97f534dda5684b
AtharvaMore <211105@iiitt.ac.in>
2022-12-01 07:37:41 +0530

    Library for data and gui handling init

---
+++ Libraries.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bits.h>
+
+class Storage{
+    //Dataset for training
+
+    //Dataset for testing
+
+    //Importing and exporting func
+};
+
+class GUI{
+    //GUI Code here
+
+
+}
